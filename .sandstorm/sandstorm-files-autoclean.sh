0505a5fb16eb685633752f6487238eb2d1b679c1
Liam Zdenek <liamzdenek@gmail.com>
2016-09-11 12:22:37 -0500

    Partial #5 - Auto-initializing the git repository on disk

---
+++ .sandstorm/sandstorm-files-autoclean.sh
@@ -0,0 +1,3 @@
+sed -i.bak '/^opt\/app\/phabricator/d' ./sandstorm-files.list
+sed -i.bak '/^opt\/app\/libphutil/d' ./sandstorm-files.list
+sed -i.bak '/^opt\/app\/arcanist/d' ./sandstorm-files.list
